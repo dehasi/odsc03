b535def1328a40026f616eece0f6abad790b9c1c
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-05-19 23:26:27 +0200

    ODSC03-1: Help Dimitry

---
+++ scalashop/src/main/java/str/Sem.java
@@ -0,0 +1,69 @@
+package str;
+
+public class Sem {
+    public String stringUtilsNoPreCheck( String arg ) {
+        char[] chars = arg.toCharArray();
+
+        int srcPos = 0;
+        int destPos = 0;
+        int length = 0;
+        boolean newSegment = false;
+        for (int i = 0; i < chars.length; ++i) {
+            if (chars[i] != '1') {
+                if (!newSegment) {
+                    srcPos = i;
+                }
+                newSegment = true;
+                ++length;
+            } else {
+                if (newSegment) {
+                    System.arraycopy(chars, srcPos, chars, destPos, length);
+                    destPos += length;
+                    length = 0;
+                    newSegment = false;
+                }
+            }
+        }
+
+        if (newSegment && arg.length() == length) {
+            return "";
+        }
+
+        if (newSegment) {
+            System.arraycopy(chars, srcPos, chars, destPos, length);
+            return new String(chars, 0, destPos + length);
+        } else {
+            return new String(chars, 0, destPos);
+        }
+    }
+
+    public String stringUtilsNoPreCheck2( String arg ) {
+        char[] chars = arg.toCharArray();
+
+        int length = chars.length;
+        int rem = 0;
+        for (int i = 0; i < length; i++) {
+            if (chars[i] == '1') {
+                int j = i+1;
+                int newi = i;
+                boolean done = false;
+                for (; j < length; j++) {
+                    if (chars[j] == '1') {
+                        newi = j+1;
+                        System.arraycopy(chars, i+1, chars, i, j-i);
+                        ++rem;
+                        done = true;
+                        break;
+                    }
+                }
+                if (!done) {
+                    System.arraycopy(chars, i+1, chars, i, length-i-1);
+                    ++rem;
+                    break;
+                }
+                i = newi;
+            }
+        }
+       return new String(chars, 0, length-rem);
+    }
+}
---
+++ scalashop/src/test/java/str/SemTest.java
@@ -0,0 +1,37 @@
+package str;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class SemTest {
+    Sem sem = new Sem();
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"2134", "234"}, {"111",""}, {"123", "23"}, {"2341", "234"}
+        });
+    }
+
+    private String input;
+
+    private String expected;
+
+    public SemTest(String input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    @Test
+    public void test() {
+        String actual = sem.stringUtilsNoPreCheck2(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
