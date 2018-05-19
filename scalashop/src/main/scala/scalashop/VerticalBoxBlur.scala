eb12ae3fe7805b19325305335b748134d2ce17fe
Ravil Galeyev <dehasi@yandex.ru>
2018-05-20 01:01:59 +0200

    ODSC03-1: Implement boxBlur

--- scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
+++ scalashop/src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -1,7 +1,6 @@
 package scalashop
 
 import org.scalameter._
-import common._
 
 object VerticalBoxBlurRunner {
 
@@ -10,7 +9,7 @@ object VerticalBoxBlurRunner {
     Key.exec.maxWarmupRuns -> 10,
     Key.exec.benchRuns -> 10,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val radius = 3
@@ -37,25 +36,35 @@ object VerticalBoxBlurRunner {
 object VerticalBoxBlur {
 
   /** Blurs the columns of the source image `src` into the destination image
-   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
-   *
-   *  Within each column, `blur` traverses the pixels by going from top to
-   *  bottom.
-   */
+    * `dst`, starting with `from` and ending with `end` (non-inclusive).
+    *
+    * Within each column, `blur` traverses the pixels by going from top to
+    * bottom.
+    */
   def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
     // TODO implement this method using the `boxBlurKernel` method
-    ???
+    val blured = for {
+      x <- from to end
+      y <- 0 to src.height
+    } yield {
+      val c = boxBlurKernel(src, x, y, radius)
+      dst.update(x, y, c)
+    }
   }
 
   /** Blurs the columns of the source image in parallel using `numTasks` tasks.
-   *
-   *  Parallelization is done by stripping the source image `src` into
-   *  `numTasks` separate strips, where each strip is composed of some number of
-   *  columns.
-   */
+    *
+    * Parallelization is done by stripping the source image `src` into
+    * `numTasks` separate strips, where each strip is composed of some number of
+    * columns.
+    */
   def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
     // TODO implement using the `task` construct and the `blur` method
-    ???
+
+    val chunk = src.width/ numTasks
+    for {
+      t <- 0 to numTasks
+    } yield task( blur(src,dst,t*chunk,(t+1)*chunk,radius))
   }
 
 }
