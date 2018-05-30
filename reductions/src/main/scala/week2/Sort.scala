cd5c3178d8e2f454dd314892603db7b43a451309
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-05-30 14:04:24 +0200

    ODSC03-2: Start implementing merge sort

--- reductions/src/main/scala/week2/Sort.scala
+++ reductions/src/main/scala/week2/Sort.scala
@@ -5,12 +5,16 @@ import common._
 import scala.util.Sorting
 
 trait Sort {
+
+
   def parMergeSort(xs: Array[Int], maxDepth: Int): Unit = {
     val ys = new Array[Int](xs.length)
 
+    def merge(src: Array[Int], dst: Array[Int], from: Int, mid: Int, until: Int): Unit = {}
+
     def sort(from: Int, until: Int, depth: Int): Unit = {
       if (depth == maxDepth) {
-        java.util.Arrays.sort(xs, from, from - until)
+        java.util.Arrays.sort(xs, from, until) //unit - from
       } else {
         val mid = (from + until) / 2
         parallel(sort(from, mid, depth + 1), sort(mid, until, depth + 1))
@@ -18,9 +22,10 @@ trait Sort {
         val flip = (maxDepth - depth) % 2 == 0
         val src = if (flip) ys else xs
         val dst = if (flip) xs else ys
-        //merge
+        merge(src, dst, from, mid, until)
       }
     }
+    sort(0,xs.length, 0)
 
   }
 }
