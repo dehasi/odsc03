aa6ece619f6f725ef0c77434bce306daa7ac0530
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-06-01 19:16:58 +0200

    ODSC03-2: Add unit tests

---
+++ reductions/src/test/scala/week2/SortTest.scala
@@ -0,0 +1,24 @@
+package week2
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class SortTest extends FunSuite {
+
+  test("merge merges a full array") {
+    val src = Array[Int](3, 4, 1, 2)
+    val dst = Array[Int](0, 0, 0, 0)
+    Sort.merge(src, dst, 0, 2, 4)
+    assert(dst sameElements Array[Int](1, 2, 3, 4))
+  }
+
+  test("merge merges a part of array") {
+    val src = Array[Int](3, 4, 1, 2, 1, 2, 5, 6)
+    val dst = Array[Int](0, 0, 0, 0, 0, 0, 0, 0)
+    Sort.merge(src, dst, 2, 4, 6)
+    assert(dst sameElements Array[Int](0, 0, 1, 1, 2, 2, 0, 0))
+  }
+
+}
