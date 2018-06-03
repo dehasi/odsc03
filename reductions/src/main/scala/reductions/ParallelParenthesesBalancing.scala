b1a4e21b7a1942fbc0805dccd9f471fd5ab396a4
Ravil Galeyev <ravil.galeyev@grandparade.co.uk>
2018-06-03 17:42:48 +0200

    ODSC03-2: Start implementing parallel balancing

--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -15,7 +15,7 @@ object ParallelParenthesesBalancingRunner {
     Key.exec.maxWarmupRuns -> 80,
     Key.exec.benchRuns -> 120,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val length = 100000000
@@ -38,14 +38,29 @@ object ParallelParenthesesBalancingRunner {
 
 object ParallelParenthesesBalancing {
 
+  sealed abstract class Tree[A]
+
+  case class Leaf[A](value: A) extends Tree[A]
+
+  case class Node[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+  def reduce[A](t: Tree[A], f: (A, A) => A): A = t match {
+    case Leaf(v) => v
+    case Node(l, r) => {
+      val (lv, rv) = parallel(reduce(l, f), reduce(r, f))
+      f(lv, rv)
+    }
+  }
+
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
+    */
+  def isOpenBrace(char: Char) = char == '('
+
+  def isClosedBrace(char: Char) = char == ')'
+
+  def isBrace(char: Char) = isOpenBrace(char) || isClosedBrace(char)
+
   def balance(chars: Array[Char]): Boolean = {
-    def isOpenBrace(char: Char) = char == '('
-
-    def isClosedBrace(char: Char) = char == ')'
-
-    def isBrace(char: Char) = isOpenBrace(char) || isClosedBrace(char)
 
     def balance(chars: Array[Char], stack: Int): Boolean = {
       if (chars.isEmpty) stack.equals(0)
@@ -59,18 +74,35 @@ object ParallelParenthesesBalancing {
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
-   */
+    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, open: Int, closed: Int): (Int, Int) = {
+      var o = open
+      var c = closed
+      var i = idx
+      while(i <  until) {
+        if (isOpenBrace(chars(i))) o += 1
+        if (isClosedBrace(chars(i))) c += 1
+        i +=1
+      }
+      (o, c)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until-from <= threshold) {
+        traverse(from,until,0,0)
+      }
+      else {
+        val mid = (from+until)/2
+        val (lv, rv) = parallel(reduce(from,mid),reduce(mid,until))
+
+        (lv._1 + rv._1, lv._2 + rv._2)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    val tuple = reduce(0, chars.length)
+    tuple._1 == tuple._2
   }
 
   // For those who want more:
