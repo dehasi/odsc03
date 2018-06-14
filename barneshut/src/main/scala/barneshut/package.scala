666d0d86847f35d1d60d7707a8d8fc51f49309bb
rgaleyev <s>
2018-06-14 15:50:06 +0200

    ODSC03-4: Add zero-check

--- barneshut/src/main/scala/barneshut/package.scala
+++ barneshut/src/main/scala/barneshut/package.scala
@@ -58,8 +58,8 @@ package object barneshut {
     val centerY: Float = (nw.centerY + ne.centerY + sw.centerY + se.centerY) / 4
     val size: Float = nw.size + ne.size + sw.size + se.size
     val mass: Float = nw.mass + ne.mass + sw.mass+se.mass
-    val massX: Float = (nw.mass*nw.massX + ne.mass*ne.massX + sw.mass*sw.massX + se.mass*se.massX)/mass
-    val massY: Float = (nw.mass*nw.massY + ne.mass*ne.massY + sw.mass*sw.massY + se.mass*se.massY)/mass
+    val massX: Float = if (mass == 0) centerX else (nw.mass*nw.massX + ne.mass*ne.massX + sw.mass*sw.massX + se.mass*se.massX)/mass
+    val massY: Float = if (mass == 0) centerY else (nw.mass*nw.massY + ne.mass*ne.massY + sw.mass*sw.massY + se.mass*se.massY)/mass
     val total: Int = nw.total + ne.total + sw.total + se.total
 
     def insert(b: Body): Fork = {
